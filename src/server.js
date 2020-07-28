008ea9cbb4f21c6f36579dd807f56f1818c5371c
tonni martinez <antoniomtz251016@outlook.com>
2020-07-27 19:40:20 -0500

    AMTZ se agrego ecma6 con babel, se agrega el git ignore y el archivo de configuaracion de babel

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-import { dummyII } from './utils/index';
-//import { deploy } from './config/environment';
+import {dummyII} from './utils/index';
 let logg = require("./utils/index");
 let http = require("http");
 
 console.log(dummyII);
 console.log(logg);
+
 
 http.createServer(function(req, res){
     res.end();
