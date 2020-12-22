818f7c8339444b4f900752765ed57920017e4720
tonni martinez <antoniomtz251016@outlook.com>
2020-12-22 15:13:44 -0600

    se deja la rama master en blanco

--- src/server.js
+++
@@ -1,7 +0,0 @@
-import {dummyII} from './utils/index';
-let logg = require("./utils/index");
-let http = require("http");
-
-http.createServer(function(req, res){
-    res.end();
-}).listen(8060);
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.logg = "EXPORTS";
-
-exports.dummy = () => {    
-    return "fin";
-};
-
-export function dummyII() {
-    return "findos";
-}
